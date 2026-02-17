e3a77c562bce1300ed3a693651d4a876dca12f3b
Kamlesh Parihar <160267650+kamleshpari@users.noreply.github.com>
2026-02-17 10:31:10 +0530

    Dtype and R operators

---
+++ LEACTURE_DSA/Secound.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+/*
+    int a=123;
+
+    cout<<a<<endl;
+  
+    char ch='A';
+    cout<<ch<<endl;
+
+    bool bl=true;
+    cout<<bl<<endl;
+
+    float f=3.14;
+    cout<<f<<endl;
+
+    double d=3.14159265358979323846;
+    cout<<d<<endl;
+
+    int size=sizeof(a);
+    cout<<"Size of a: "<<size<<" bytes"<<endl;
+
+
+int a='a';
+cout<<a<<endl;
+
+char ch=98;
+cout<<ch<<endl;
+
+char ch1=123456;
+cout<<ch1<<endl;
+
+unsigned int a=-1;
+cout<<a<<endl;
+
+
+
+// oprators 
+int a=2/5;
+cout<<a<<endl;
+
+int a=2;
+int b=5;
+
+bool result=a>b;
+cout<<result<<endl;
+
+bool result1=a<b;
+cout<<result1<<endl;
+
+bool result2= a==b;
+cout<<result2<<endl;
+
+bool result3=a>=b;
+cout<<result3<<endl;
+
+bool result4=a<=b;
+cout<<result4<<endl;
+
+bool result5=a !=b;
+cout<<result5<<endl;
+*/
+int a=10;
+cout<<!a<<endl;
+
+
+}
