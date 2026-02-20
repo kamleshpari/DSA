5ca5ac7f982ab75f680019772b1a62be9d686d3c
Kamlesh Parihar <160267650+kamleshpari@users.noreply.github.com>
2026-02-20 10:11:01 +0530

    while loops

---
+++ LEACTURE_DSA/Leacture3.cpp
@@ -0,0 +1,198 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   /* int a;
+    cin>>a;
+
+    //cout<<"The value of a is: "<<a<<endl;
+
+    if(a>0){
+        cout<<"The number is positive."<<endl;
+    }
+    else if(a<0){
+        cout<<"The number is negative."<<endl;
+    }
+    else{
+        cout<<"The number is zero."<<endl;
+    }
+
+
+int a,b;
+cin>>a>>b;
+if(a>b){
+    cout<<"a is greater than b."<<endl;
+}
+else if(a<b){
+    cout<<"a is less than b."<<endl;
+}
+else{
+    cout<<"a and b are equal."<<endl;
+
+}
+
+int a,b;
+cout<<"Enter the value of a: ";
+cin>>a;
+cout<<"Enter the value of b: ";
+cin>>b;
+
+if(a>b){
+    cout<<"a is greater than b."<<endl;
+}
+else if(a<b){
+    cout<<"a is less than b."<<endl;
+}
+else{
+    cout<<"a and b are equal."<<endl;
+}
+ //a=cin.get();
+// cout<<"The value of a is: "<<a<<endl;
+
+int a;
+cout<<"enter the a value of a:";
+cin>>a;
+if(a>0){
+    cout<<"The number is positive."<<endl;
+}
+else if(a<0){
+    cout<<"The number is negative."<<endl;
+}
+else{
+    cout<<"The number is zero."<<endl;
+}
+
+
+
+int a;
+cout<<"Enter the value of a: ";
+cin>>a;
+
+if(a==9){
+    cout<<"The value of a is 9."<<endl;
+}
+if(a>0){
+    cout<<"The number is positive."<<endl;
+
+}else{
+    cout<<"The number is negative."<<endl;
+}
+    
+   int a=2;
+   int b=a+1;
+
+   if((a=3)==b){
+    cout<<"Condition is true."<<endl;
+   }
+   else{
+    cout<<"Condition is false."<<a+1<<endl;
+   }
+
+   int a=24;
+
+   if(a> 20){
+    cout<<"i love vanu"<<endl;
+   }
+   else if(a==24){
+    cout<<"i love vanu more than vanu"<<endl;
+   }
+   else{
+    cout<<"i love vanu less than kamali"<<endl;
+   }
+
+  
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    if (ch >= 'A' && ch <= 'Z') {
+        cout << "Uppercase Letter" << endl;
+    } 
+    else if (ch >= 'a' && ch <= 'z') {
+        cout << "Lowercase Letter" << endl;
+    } 
+    else if (ch >= '0' && ch <= '9') {
+        cout << "Numeric Digit" << endl;
+    } 
+    else {
+        cout << "Special Character" << endl;
+    }
+
+    return 0;
+    
+
+    //Looping concept
+
+    //priint numbers from 1 to n
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<i<<" ";
+        i++;
+    }
+
+int n;
+cout<<"enter the value of n";
+cin>>n;
+int i=1;
+int sum=0;
+while(i<=n){
+    sum=sum+i;
+    i++;
+    
+}
+cout<<"Sum is: "<<sum<<endl;
+
+
+//n  find  sum of all envn numbers from 1 to n
+
+int n;
+cout<<"enter the value of n:";
+cin>>n;
+int i =1;
+int sum=0;
+
+while(i<=n){
+    if(i%2==0){
+        sum=sum+i;
+    cout<<i<<" ";
+    }
+    i++;
+
+}
+cout<<"Sum of even numbers is: "<<sum<<endl;
+
+
+    double fahrenheit, celsius;
+
+    cout << "Enter temperature in Fahrenheit: ";
+    cin >> fahrenheit;
+
+    celsius = (fahrenheit - 32) * 5.0 / 9.0;
+
+    cout << "Temperature in Celsius: " << celsius << endl;
+
+    return 0;
+    
+// prime number from 1 to n
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    int i=2;
+
+    while(i<n){
+        if(n%i==0){
+            cout<<"Not a prime number."<< i <<endl;
+            
+        }
+        else{
+            cout<<"Prime number."<< i <<endl;
+        }
+        i++;
+    }
+
+*/
+}
+
